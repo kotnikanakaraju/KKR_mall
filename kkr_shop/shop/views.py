fa12788dbde38fcd2df3c014d331318604381838
kotnikanakaraju <kanakarajukotni@gmail.com>
2023-09-29 19:42:13 +0530

    views.py

--- kkr_shop/shop/views.py
+++ kkr_shop/shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . models import store,product
 
-# Create your views here.
+def store_detail(request,store_id):
+    store =store.objects.get(pk=store_id)
+    products=product.objects.filter(store=store)
+    return render(request,'shop/store_detail.html',{'store':store,'products':products})
